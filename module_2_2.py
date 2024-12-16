edc724105be00fca290e653d3da738bd3d4854da
Tim <art.zakharov92@gmail.com>
2024-12-16 22:10:14 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+a = int(input('first number:'))
+b = int(input('second number:'))
+c = int(input('third number'))
+if a==b and a==c and b==c :
+    print(3)
+if a==b or a==c or b==c:
+    print(2)
+else:
+    print('Zero')
+
